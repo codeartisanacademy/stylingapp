3daca594282c489332eae31c882b181b3b644808
wahyudi <wahyudi.coding@gmail.com>
2022-05-14 13:58:51 +0700

    detail page

--- src/components/screens/Detail.js
+++ src/components/screens/Detail.js
@@ -1,10 +1,30 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
+import { SliderBox } from "react-native-image-slider-box";
+import { baseStyle } from '../../styles/style';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Detail = () => {
+
+  const images = [
+    "https://cdna.lystit.com/520/650/n/photos/mytheresa/09aa1bc4/polo-ralph-lauren-white-Court-Leather-Sneakers.jpeg",
+    "https://cdna.lystit.com/520/650/n/photos/mytheresa/565d0e13/polo-ralph-lauren-white-Court-Leather-Sneakers.jpeg",
+    "https://cdna.lystit.com/520/650/n/photos/mytheresa/307643b4/polo-ralph-lauren-white-Court-Leather-Sneakers.jpeg",
+    "https://cdna.lystit.com/520/650/n/photos/mytheresa/f4992012/polo-ralph-lauren-white-Baskets-Court-en-cuir.jpeg"
+  ]
+
   return (
     <View>
-      <Text>Detail</Text>
+      <SliderBox images={images} sliderBoxHeight={400} />
+      <View style={[baseStyle.container, baseStyle.colView]}>
+          <View>
+            <Text style={baseStyle.productTitle}>NIKE</Text>
+            <Text>Air Max Dawn</Text>
+          </View>
+          <View>
+            <FontAwesome name="heart-o" size={24} color="black" />
+          </View>
+      </View>
     </View>
   )
 }
